3b7ce61ce469e2c95d30750f94492761c4a4127e
Beo <127577359+NghiaLeopard@users.noreply.github.com>
2023-07-31 02:03:20 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ import axiosClient from './api/axiosClient'
 console.log('hello from main.js')
 
 async function main() {
-  // const response = await axiosClient.get('/posts')
   const response = await axiosClient.get('/posts')
   console.log(response)
 }
